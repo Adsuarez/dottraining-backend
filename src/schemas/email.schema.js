51dc962055f97fbabec8b49a0ea0868997636343
Adsuarez <adsuarezq@gmail.com>
2023-05-08 17:00:52 -0500

    add validation of email by database

--- src/schemas/email.schema.js
+++ src/schemas/email.schema.js
@@ -1,3 +1,5 @@
+import { isEmailInDB } from '#Helpers/isEmailInDB.js'
+
 export const emailSchema = {
 	required: true,
 	type: 'string',
@@ -8,5 +10,7 @@ export const validateEmail = async (email) => {
 	if (emailSchema.required && !email) return false
 	if (typeof email !== typeof emailSchema.type) return false
 	if (emailSchema.format.test(email) !== true) return false
+	const existEmailInDB = await isEmailInDB(email)
+	if (existEmailInDB === false) return false
 	return true
 }
