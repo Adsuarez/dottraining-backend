57c83f6e148a208592cea1c4468b2b4ec90d74d2
Adsuarez <adsuarezq@gmail.com>
2023-05-10 17:40:44 -0500

    add middleware to read all request data

--- src/config/express.js
+++ src/config/express.js
@@ -5,6 +5,19 @@ const expressApp = express()
 
 console.clear()
 expressApp.use(express.json())
+expressApp.use((req, res, next) => {
+	console.log('\n\n👀 Reading information 🖥️')
+	console.log('method: ', req.method)
+	console.log('path: ', req.path)
+	console.log('body: ', req.body)
+	console.log('\nFinished reading ✅\n\n')
+	next()
+})
 expressApp.use('/api', userRoutes)
+expressApp.use((error, req, res, next) => {
+	return res.status(500).json({
+		errorMessage: error.message,
+	})
+})
 
 export default expressApp
