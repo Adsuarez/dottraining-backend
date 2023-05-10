3166be4c3b6d8619d2ab4ebbf19c61d251060567
Adsuarez <adsuarezq@gmail.com>
2023-05-10 17:42:07 -0500

    add constanst to nonexisting emails and users in database

--- tests/constants.js
+++ tests/constants.js
@@ -3,3 +3,20 @@ import { HOST_URL, PORT } from '#Config/environment.js'
 export const BASE_URL = `${HOST_URL}:${PORT}/`
 export const API_USERS = 'api/users/'
 export const API_USERS_LOGIN = 'api/users/login'
+export const nonexistingEmailInDB = [
+	'test@example.com',
+	'admin@dot.com',
+	'something@dottraining.com',
+	'aprendiz@ejemplo.com',
+	'aprendiz01@dottraining.com',
+	'aprendiz02@ejemplo.com',
+]
+export const existingEmail = ['admin@dottraining.com', 'aprendiz01@ejemplo.com']
+export const nonexistingUserInDB = [
+	{ email: nonexistingEmailInDB[0], password: 'Testing1234' },
+	{ email: nonexistingEmailInDB[1], password: 'Testing1234' },
+	{ email: nonexistingEmailInDB[2], password: 'Testing1234' },
+	{ email: nonexistingEmailInDB[3], password: 'Testing1234' },
+	{ email: nonexistingEmailInDB[4], password: 'Testing1234' },
+	{ email: nonexistingEmailInDB[5], password: 'Testing1234' },
+]
