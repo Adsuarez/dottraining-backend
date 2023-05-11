c54f47578229a261e2ce425e796dc1be52822055
Adsuarez <adsuarezq@gmail.com>
2023-05-11 17:24:19 -0500

    remove logs and separate extract of body keys from conditional to clearlity

--- src/dto/validateUser.dto.js
+++ src/dto/validateUser.dto.js
@@ -1,17 +1,15 @@
-import { badRequest } from '#Helpers/errors.js'
+import { badRequest, unauthorized } from '#Helpers/errors.js'
 import { AMOUNT_OF_VALUES_LOGIN } from '#Config/constants.js'
 import { validateEmail } from '#Schemas/email.schema.js'
 
 export function validateUserDTO(req, res, next) {
-	console.log('\n\n\nInside validateUserDTO 👍\n\n')
 	const { body } = req
 	const { email } = body
-	if (Object.keys(body).length !== AMOUNT_OF_VALUES_LOGIN)
-		return badRequest(res)
+	const amountInBody = Object.keys(body).length
+	if (amountInBody !== AMOUNT_OF_VALUES_LOGIN) return badRequest(res)
 
 	validateEmail(email).then((check) => {
-		console.log({ check })
-		if (check === false) return res.status(402).json()
+		if (check === false) return unauthorized(res)
 		next()
 	})
 }
