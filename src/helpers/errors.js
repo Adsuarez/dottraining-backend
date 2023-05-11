5060c42391f63a62fada68ee2a0e889ca8d08992
Adsuarez <adsuarezq@gmail.com>
2023-05-11 17:22:48 -0500

    create function to standardize the response

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -3,3 +3,7 @@ import { jsonErrorResponse } from '#Config/constants.js'
 export const badRequest = (res) => {
 	return res.status(400).json(jsonErrorResponse[400])
 }
+
+export const unauthorized = (res) => {
+	return res.status(401).json()
+}
