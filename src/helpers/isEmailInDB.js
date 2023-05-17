4ae306940ecc20d6f2e12b42755c991872dc8bde
Adsuarez <adsuarezq@gmail.com>
2023-05-17 17:04:27 -0500

    solve error with asynchronous activity

--- src/helpers/isEmailInDB.js
+++ src/helpers/isEmailInDB.js
@@ -1,20 +1,10 @@
 import { pool } from '#Config/db.js'
 
 export const isEmailInDB = async (email) => {
-	/* const [[rows]] = await pool.query(
+	const [[result]] = await pool.query(
 		'SELECT `email` FROM `user` WHERE `email` = ?',
 		[email]
 	)
-	console.log({ rows }) */
-	return pool
-		.promise()
-		.query('SELECT `email` FROM `user` WHERE `email` = ?', [email])
-		.then(([[rows]]) => {
-			if (rows) return true
-		})
-		.catch(console.log)
-	// .then(() => pool.end())
-	/* .catch((error) => {
-			if (error.message.includes(`add new command when`)) return null
-		}) */
+	if (!result) return false
+	return true
 }
