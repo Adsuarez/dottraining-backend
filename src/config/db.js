1b94252668d63fb4c2f9514bbb8311be904f26d6
Adsuarez <adsuarezq@gmail.com>
2023-05-17 17:03:26 -0500

    change library to solve error with DB connection promise query

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-import { createPool } from 'mysql2'
+import { createPool } from 'mysql2/promise'
 import { HOST, USER, PASSWORD, DB_PORT, DATABASE } from '#Config/environment.js'
 
 export const pool = createPool({
