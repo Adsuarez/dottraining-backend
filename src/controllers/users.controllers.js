28f3e37d5942eb27aeb9b50ffc51fa8f8cad6b21
Adsuarez <adsuarezq@gmail.com>
2023-05-18 12:56:51 -0500

    response with 202 status code when all validation was ok and begin jwt ...Pending continue

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,7 @@
+import jwt from 'jsonwebtoken'
+import { getUserFromDB } from '#Helpers/getUserFromDB.js'
+import { JWT_SECRET_KEY } from '#Config/environment.js'
+
 export const getUsers = (req, res, next) => {
 	return res.json([
 		{
@@ -11,6 +15,9 @@ export const createUser = (req, res, next) => {
 	return res.json(req.body)
 }
 
-export const validateUser = (req, res, next) => {
-	return res.json(req.body)
+export const validateUser = async (req, res, next) => {
+	const { email } = req.body
+	const userForToken = await getUserFromDB(email)
+	const token = jwt.sign(userForToken, JWT_SECRET_KEY)
+	return res.status(202).json(token)
 }
