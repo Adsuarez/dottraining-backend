67bdb8dcb7c352af8e2ff159cfcc9b37385042ca
Adsuarez <adsuarezq@gmail.com>
2023-05-18 12:55:24 -0500

    add test for 202 status response when email and password are correct

--- tests/integration/validateUser.test.js
+++ tests/integration/validateUser.test.js
@@ -6,6 +6,7 @@ import {
 	API_USERS_LOGIN,
 	nonexistingUserInDB,
 	existingEmailWithWrongPassword,
+	existingUserInDB,
 } from '#Tests/constants.js'
 import { jsonErrorResponse } from '#Config/constants.js'
 
@@ -17,7 +18,7 @@ const wrongAmountOfValues = [
 	{},
 ]
 
-describe.skip(`Testing -> POST ${API_USERS_LOGIN}`, async () => {
+describe(`Testing -> POST ${API_USERS_LOGIN}`, async () => {
 	describe('given incorrect information in body', async () => {
 		it('should respond with 400 status code with nulish data', async () => {
 			for (const data of nulishData) {
@@ -53,12 +54,22 @@ describe.skip(`Testing -> POST ${API_USERS_LOGIN}`, async () => {
 				assert.strictEqual(response?.statusCode, 401)
 			}
 		})
-		it.skip('should response 401 status code with existing email in database but wrong password', async () => {
+		it('should response 401 status code with existing email in database but wrong password', async () => {
 			for (const data of existingEmailWithWrongPassword) {
 				const response = await request(BASE_URL)
 					.post(API_USERS_LOGIN)
 					.send(data)
 				assert.strictEqual(response?.statusCode, 401)
+			}
+		})
+	})
+	describe('given authorized email and password', async () => {
+		it('should respond 202 status code with existing email and password', async () => {
+			for (const data of existingUserInDB) {
+				const response = await request(BASE_URL)
+					.post(API_USERS_LOGIN)
+					.send(data)
+				assert.strictEqual(response?.statusCode, 202)
 			}
 		})
 	})
