aac751248382e67c2964153aa544b066e1d24979
Adsuarez <adsuarezq@gmail.com>
2023-05-18 12:48:34 -0500

    create constant to jwt secret key

--- src/config/environment.js
+++ src/config/environment.js
@@ -9,3 +9,4 @@ export const USER = process.env.DB_USER
 export const PASSWORD = process.env.DB_PASSWORD
 export const DATABASE = process.env.DB_DATABASE
 export const DB_PORT = process.env.DB_PORT
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
