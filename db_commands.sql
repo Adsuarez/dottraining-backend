8275451c54f3ed4a6f760d32540dd044a33ca468
Adsuarez <adsuarezq@gmail.com>
2023-05-19 12:41:10 -0500

    add commands to save upgrade of user table and new training table

--- db_commands.sql
+++ db_commands.sql
@@ -6,10 +6,12 @@ CREATE DATABASE dottraining_db;
 
 USE dottraining_db;
 
+/*-----------User table commands----------*/
 CREATE TABLE IF NOT EXISTS `user` (
 	`id` INT NOT NULL UNIQUE AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL UNIQUE,
+    `permissions` INT NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -17,12 +19,36 @@ DESCRIBE user;
 
 SELECT * FROM user;
 
-INSERT INTO user (email, password) VALUES ("admin@dottraining.com", "AdminPassword123");
+INSERT INTO user (email, password, permissions) VALUES ("test_admin@dottraining.com", "AdminPassword123", 0);
 
-INSERT INTO user (email, password) VALUES ("aprendiz01@ejemplo.com", "AprendizPassword789");
+INSERT INTO user (email, password, permissions) VALUES ("test_aprendiz@dottraining.com", "AprendizPassword789", 0);
 
 /* DELETE FROM user WHERE email = "admin@dottraining.com";
 
 DELETE FROM user WHERE email = "aprendiz01@ejemplo.com"; 
 
-SELECT `id`, `email` FROM `user` WHERE `email` = "admin@dottraining.com"; */
+SELECT `id`, `email` FROM `user` WHERE `email` = "admin@dottraining.com";
+
+DROP TABLE user; */
+
+/*------------Training table commands-------------*/
+CREATE TABLE IF NOT EXISTS `training` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `createdBy` INT,
+    `name` VARCHAR(100) NOT NULL,
+    `quotas` INT,
+    `studyDays` JSON DEFAULT NULL,
+    `enrolledStudents` JSON DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`createdBy`) REFERENCES user(`id`)
+);
+
+DESCRIBE training;
+
+SELECT * FROM training;
+
+/*Correct JSON valid text
+"studyDays": "[{\"date\":\"2023-05-19\",\"startTime\":\"17:15\",\"endTime\":\"19:15\"}]"
+*/
+
+DELETE FROM training WHERE id >= 15;
