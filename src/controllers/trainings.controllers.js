92d4fecdb27cba688b081bd7e20ae2521ca4674a
Adsuarez <adsuarezq@gmail.com>
2023-05-19 12:46:56 -0500

    completed endpoint to create new training

--- src/controllers/trainings.controllers.js
+++ src/controllers/trainings.controllers.js
@@ -1,4 +1,21 @@
+import { saveTrainingInDB } from '#Helpers/saveTrainingInDB.js'
+import { jsonErrorResponse } from '#Config/constants.js'
+import { notAceptable } from '#Helpers/errors.js'
+
 export const createTraining = (req, res, next) => {
 	const { body, userId } = req
-	console.log({ body, userId })
+	const { name, quotas, studyDays } = body
+	const newTraining = {
+		createdBy: userId,
+		name,
+		quotas,
+		studyDays,
+		enrolledStudents: null,
+	}
+	saveTrainingInDB(newTraining)
+		.then((savedOk) => {
+			if (!savedOk) return res.status(406).json(jsonErrorResponse[406])
+			return res.sendStatus(201)
+		})
+		.catch(({ code }) => notAceptable(code, res))
 }
