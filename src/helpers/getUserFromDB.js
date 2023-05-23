a8f6d87b93268d1d28b220ad930b8baa5bd4b982
Adsuarez <adsuarezq@gmail.com>
2023-05-23 15:23:15 -0500

    add permissions to get from user in database

--- src/helpers/getUserFromDB.js
+++ src/helpers/getUserFromDB.js
@@ -2,7 +2,7 @@ import { pool } from '#Config/db.js'
 
 export const getUserFromDB = async (email) => {
 	const [[result]] = await pool.query(
-		'SELECT `id`, `email` FROM `user` WHERE `email` = ?',
+		'SELECT `id`, `email`, `permissions` FROM `user` WHERE `email` = ?',
 		[email]
 	)
 	return result
