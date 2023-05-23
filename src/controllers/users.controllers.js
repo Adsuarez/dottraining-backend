01b2ee0570a61fb1ff0e53c742695258b49854ad
Adsuarez <adsuarezq@gmail.com>
2023-05-23 15:21:05 -0500

    add new helper to create username and send aditional data in response

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { getUserFromDB } from '#Helpers/getUserFromDB.js'
 import { JWT_SECRET_KEY } from '#Config/environment.js'
+import { createDefaultUsername } from '#Helpers/createDefaultUsername.js'
 
 export const getUsers = (req, res, next) => {
 	return res.json([
@@ -21,5 +22,8 @@ export const validateUser = async (req, res, next) => {
 	const token = jwt.sign(userForToken, JWT_SECRET_KEY, {
 		expiresIn: 60 * 60 * 24 * 7,
 	})
-	return res.status(202).json({ token })
+	const username = createDefaultUsername(email)
+	const userId = userForToken.id
+	const permissions = userForToken.permissions
+	return res.status(202).json({ token, username, userId, permissions })
 }
