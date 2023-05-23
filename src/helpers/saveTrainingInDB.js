4730af03ec29ee41b9bf9f271236303f672b7524
Adsuarez <adsuarezq@gmail.com>
2023-05-23 17:06:12 -0500

    organize data to solve SQL syntax JSON error ...OK

--- src/helpers/saveTrainingInDB.js
+++ src/helpers/saveTrainingInDB.js
@@ -1,16 +1,18 @@
 import { pool } from '#Config/db.js'
 
 export const saveTrainingInDB = async (newTraining) => {
-	const { createdBy, name, quotas, studyDays } = newTraining
+	const { createdBy, name, quotas, date, startTime, endTime } = newTraining
+	const studyDays = [{ date, startTime, endTime }]
 	const parseStudyDays = JSON.stringify(studyDays)
-	console.log({ parseStudyDays })
-	// "[{\"date\":\"2023-05-19\",\"startTime\":\"17:15\",\"endTime\":\"19:15\"}]"
+	const parserQuotas = Number(quotas)
+	console.log(parseStudyDays)
 	return pool
 		.query(
 			'INSERT INTO training (createdBy, name, quotas, studyDays) VALUES (?, ?, ?, ?)',
-			[createdBy, name, quotas, parseStudyDays]
+			[createdBy, name, parserQuotas, parseStudyDays]
 		)
 		.then(([row]) => {
+			console.log({ row })
 			if (!row.affectedRows) return false
 			return true
 		})
