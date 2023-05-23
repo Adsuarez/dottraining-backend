960d0df3c15ffe0e83d4e91a05357d4e35715453
Adsuarez <adsuarezq@gmail.com>
2023-05-23 15:23:59 -0500

    add information to try solve error with JSON sintax in SQL query ...pending

--- src/helpers/saveTrainingInDB.js
+++ src/helpers/saveTrainingInDB.js
@@ -1,11 +1,14 @@
 import { pool } from '#Config/db.js'
 
 export const saveTrainingInDB = async (newTraining) => {
-	const { createdBy, name, quotas, studyDays, enrolledStudents } = newTraining
+	const { createdBy, name, quotas, studyDays } = newTraining
+	const parseStudyDays = JSON.stringify(studyDays)
+	console.log({ parseStudyDays })
+	// "[{\"date\":\"2023-05-19\",\"startTime\":\"17:15\",\"endTime\":\"19:15\"}]"
 	return pool
 		.query(
-			'INSERT INTO training (createdBy, name, quotas, studyDays, enrolledStudents) VALUES (?, ?, ?, ?, ?)',
-			[createdBy, name, quotas, studyDays, enrolledStudents]
+			'INSERT INTO training (createdBy, name, quotas, studyDays) VALUES (?, ?, ?, ?)',
+			[createdBy, name, quotas, parseStudyDays]
 		)
 		.then(([row]) => {
 			if (!row.affectedRows) return false
