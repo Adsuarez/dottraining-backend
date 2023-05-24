6dfab9aae1b202f39e2f1f400d223d8100f3060d
Adsuarez <adsuarezq@gmail.com>
2023-05-24 17:00:12 -0500

    add route and dto in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,12 +5,13 @@ import {
 	validateUser,
 } from '#Controllers/users.controllers.js'
 import { validateUserDTO } from '#Dto/validateUser.dto.js'
+import { validateNewUserDTO } from '#Dto/validateNewUser.dto.js'
 
 const router = Router()
 
 router.get('/users', getUsers)
 
-router.post('/users', createUser)
+router.post('/users', validateNewUserDTO, createUser)
 
 router.post('/users/login', validateUserDTO, validateUser)
 
