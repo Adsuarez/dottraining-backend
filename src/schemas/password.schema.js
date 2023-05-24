4af845883f24058f122393d5aaf5521d82cbe293
Adsuarez <adsuarezq@gmail.com>
2023-05-24 16:59:10 -0500

    create password validation to new user

--- src/schemas/password.schema.js
+++ src/schemas/password.schema.js
@@ -14,3 +14,10 @@ export const validatePassword = async (password, email) => {
 	if (passwordCorrespondsEmail === false) return false
 	return true
 }
+
+export const validatePasswordToRegister = async (password) => {
+	if (passwordSchema.required && !password) return false
+	if (typeof passwordSchema.type !== typeof password) return false
+	if (passwordSchema.format.test(password) === false) return false
+	return true
+}
