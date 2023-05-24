1a0914f3532e67d095b7f9c051cb6ad5c5ac9671
Adsuarez <adsuarezq@gmail.com>
2023-05-24 12:08:51 -0500

    add middleware and route in trainings routes

--- src/routes/training.routes.js
+++ src/routes/training.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { createTraining } from '#Controllers/trainings.controllers.js'
+import {
+	createTraining,
+	getTrainings,
+} from '#Controllers/trainings.controllers.js'
 import { userExtractor } from '#Middleware/userExtractor.js'
 
 const router = Router()
 
 router.post('/trainings', userExtractor, createTraining)
+router.get('/trainings', getTrainings)
 
 export default router
