fd0aafcd2af004f740dc4df89a6320ccc0a7f070
Adsuarez <adsuarezq@gmail.com>
2023-05-24 10:05:33 -0500

    remove logs used to debugging

--- src/helpers/saveTrainingInDB.js
+++ src/helpers/saveTrainingInDB.js
@@ -5,14 +5,12 @@ export const saveTrainingInDB = async (newTraining) => {
 	const studyDays = [{ date, startTime, endTime }]
 	const parseStudyDays = JSON.stringify(studyDays)
 	const parserQuotas = Number(quotas)
-	console.log(parseStudyDays)
 	return pool
 		.query(
 			'INSERT INTO training (createdBy, name, quotas, studyDays) VALUES (?, ?, ?, ?)',
 			[createdBy, name, parserQuotas, parseStudyDays]
 		)
 		.then(([row]) => {
-			console.log({ row })
 			if (!row.affectedRows) return false
 			return true
 		})
