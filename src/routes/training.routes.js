117cd5a1c9ebcdcf8f7c3814d541e1e6bab681d2
Adsuarez <adsuarezq@gmail.com>
2023-05-25 13:01:21 -0500

    add route and middleware to enroll students in a training

--- src/routes/training.routes.js
+++ src/routes/training.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import {
 	createTraining,
 	getTrainings,
+	enrollTraining,
 } from '#Controllers/trainings.controllers.js'
 import { userExtractor } from '#Middleware/userExtractor.js'
 
@@ -9,5 +10,6 @@ const router = Router()
 
 router.post('/trainings', userExtractor, createTraining)
 router.get('/trainings', getTrainings)
+router.post('/trainings/enroll', userExtractor, enrollTraining)
 
 export default router
