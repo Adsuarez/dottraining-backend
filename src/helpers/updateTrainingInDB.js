d968fe0900cfaed93cee392c0cb22a068e07cbee
Adsuarez <adsuarezq@gmail.com>
2023-05-26 16:57:41 -0500

    change contrat to add action and reuse service to enroll and cancel users subscribe and apply quotas changes

--- src/helpers/updateTrainingInDB.js
+++ src/helpers/updateTrainingInDB.js
@@ -1,23 +1,39 @@
 import { pool } from '#Config/db.js'
 import { getEnrolledStudentsFromDB } from '#Helpers/getEnrolledStudentsFromDB.js'
 
-export const updateTrainingInDB = async ({ trainingId, userId }) => {
+export const updateTrainingInDB = async ({ trainingId, userId, action }) => {
+	let [[{ quotas }]] = await pool.query(
+		'SELECT quotas FROM training WHERE id = ?',
+		[trainingId]
+	)
+
+	console.log({ quotas })
 	return getEnrolledStudentsFromDB({ trainingId }).then((array) => {
 		let newArray
-		if (array) {
-			const userAlreadyExists = array.includes(userId)
-			if (userAlreadyExists) return false
-			newArray = array.concat(userId)
-		} else {
-			newArray = [userId]
+
+		if (action === 'enroll') {
+			if (array) {
+				const userAlreadyExists = array.includes(userId)
+				if (userAlreadyExists) return false
+				newArray = array.concat(userId)
+				quotas--
+			} else {
+				newArray = [userId]
+				quotas--
+			}
+		}
+
+		if (action === 'cancel') {
+			newArray = array.filter((users) => users !== userId)
+			quotas++
 		}
 
 		return pool
 			.query(
-				'UPDATE training SET enrolledStudents = IFNULL("[?]", enrolledStudents) WHERE id = ?',
-				[newArray, trainingId]
+				'UPDATE training SET enrolledStudents = IFNULL("[?]", enrolledStudents), quotas = ? WHERE id = ?',
+				[newArray, quotas, trainingId]
 			)
-			.then((rows) => {
+			.then(([rows]) => {
 				if (!rows.affectedRows) return false
 				return true
 			})
