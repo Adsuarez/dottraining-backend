0cbd09ea5776a72f1ef2f4558f61579d29a74db6
Adsuarez <adsuarezq@gmail.com>
2023-05-26 09:04:55 -0500

    create validation of enrolled strudents getted from database and update with new user enrolled

--- src/helpers/updateTrainingInDB.js
+++ src/helpers/updateTrainingInDB.js
@@ -2,7 +2,24 @@ import { pool } from '#Config/db.js'
 import { getEnrolledStudentsFromDB } from '#Helpers/getEnrolledStudentsFromDB.js'
 
 export const updateTrainingInDB = async ({ trainingId, userId }) => {
-	getEnrolledStudentsFromDB(trainingId).then((array) => {
-		// pending insert new user in array
+	return getEnrolledStudentsFromDB({ trainingId }).then((array) => {
+		let newArray
+		if (array) {
+			const userAlreadyExists = array.includes(userId)
+			if (userAlreadyExists) return false
+			newArray = array.concat(userId)
+		} else {
+			newArray = [userId]
+		}
+
+		return pool
+			.query(
+				'UPDATE training SET enrolledStudents = IFNULL("[?]", enrolledStudents) WHERE id = ?',
+				[newArray, trainingId]
+			)
+			.then((rows) => {
+				if (!rows.affectedRows) return false
+				return true
+			})
 	})
 }
