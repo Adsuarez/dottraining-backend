81f64e15b28e289d9b588718776c6b51fe4b6ffb
Adsuarez <adsuarezq@gmail.com>
2023-05-30 17:17:30 -0500

    remove port and add ssl in pool to database ...pending test

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
 import { createPool } from 'mysql2/promise'
-import { HOST, USER, PASSWORD, DB_PORT, DATABASE } from '#Config/environment.js'
+import { HOST, USER, PASSWORD, DATABASE } from '#Config/environment.js'
 
 export const pool = createPool({
 	host: HOST,
 	user: USER,
 	password: PASSWORD,
-	port: DB_PORT,
 	database: DATABASE,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 })
